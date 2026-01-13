26ee5403d5d5375028ecf185e4d90b694beb1fbd
Andrés Iñiguez <andy.iniguez@hotmail.com>
2026-01-13 18:16:56 -0300

    importo todo y genero greeting para que el prop que uso en ILC funcione

--- Primera entrega/src/App.jsx
+++ Primera entrega/src/App.jsx
@@ -1,1 +1,13 @@
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenido a Fox Stickers! Elegí tus favoritos." />
+    </div>
+  );
+}
+
 export default App;
