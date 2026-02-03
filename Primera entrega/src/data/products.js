7c2a65394d150fa2da09704a98d84caa7736a741
Andrés Iñiguez <andy.iniguez@hotmail.com>
2026-02-03 19:19:07 -0300

    creo servicio mock con promise

---
+++ Primera entrega/src/data/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: "1",
+    title: "Sticker Naruto",
+    category: "anime",
+    price: 1200,
+    description: "Vinilo premium, resistente al agua.",
+    stock: 10,
+    pictureUrl: "https://via.placeholder.com/600x400?text=Naruto",
+  },
+  {
+    id: "2",
+    title: "Sticker PlayStation",
+    category: "gaming",
+    price: 1500,
+    description: "Ideal para notebook o consola.",
+    stock: 8,
+    pictureUrl: "https://via.placeholder.com/600x400?text=PlayStation",
+  },
+  {
+    id: "3",
+    title: "Sticker JavaScript",
+    category: "programacion",
+    price: 1000,
+    description: "JS vibes 🔥",
+    stock: 15,
+    pictureUrl: "https://via.placeholder.com/600x400?text=JavaScript",
+  },
+];
